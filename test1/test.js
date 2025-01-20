eef4b71bc3ce364859fc82f3fa2a28cfe898b0af
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-01-20 09:10:30 -0300

    new one

---
+++ test1/test.js
@@ -0,0 +1,19 @@
+
+
+var compose = function(functions) {
+    console.log(functions)
+    // return function(x) {
+        
+    // }
+};
+
+compose([x => x + 1, x => 2 * x])
+// const fn = compose([x => x + 1, x => 2 * x])
+// console.log(fn(4))
+
+
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
