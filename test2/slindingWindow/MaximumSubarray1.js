1d9fee1c9b3afabe41af4d611ae183ba69548347
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-03-14 17:52:38 -0300

    .

--- test2/slindingWindow/MaximumSubarray1.js
+++ test2/slindingWindow/MaximumSubarray1.js
@@ -1,22 +1,29 @@
 
+// Finding the maximum value: Initialize with -Infinity so any real number replaces it.
+
+// Finding the minimum value: Initialize with Infinity so any real number replaces it
+
+
+// we need a sum, max, start and end pointers to solve this problem
+// end pointer is the I -> the iterable variable from FOR
 
 function findMax(nums, k){
-    let max = 0
+    let max = -Infinity
     let sum = 0
     let windowStart = 0
 
-    for(let i = 0;i < nums.length;i++){
-        sum += nums[i]
-        if(i - windowStart === k - 1){
-            // return `${i} - ${windowStart} === ${k}`
-            let average = sum / k
-            max = Math.max(max, average)
-            sum -= nums[windowStart]
-            windowStart++
+    for(let i = 0;i < nums.length;i++){ // 0 1 2 3 4 5
+        // 
+        sum += nums[i] // 1/13/8/2 virou 1 -> 51, virou 39 -> 42
+        // 5    2                3
+        if(i - windowStart === k - 1){//isso que mantem os numeros dentro do padrao (a cada (k) numeros.. nesse caso a cada 4 faz a soma).
+            
+            let average = sum / k // 10.5
+            max = Math.max(max, average) // 12.75
+            sum -= nums[windowStart]// 1/39  // we decrement the fist element
+            windowStart++// 1/2 // then the second element becomes the first
         }
     }
-    return max
-    
+    return max   
 }
-
 console.log(findMax([1, 12, -5, -6, 50, 3], 4))
--- test2/slindingWindow/test.js
+++ test2/slindingWindow/test.js
@@ -1,24 +1,17 @@
 
 // Problem: Given an array and an integer k, find the maximum sum of any contiguous subarray of size k.
 
+
 function maxSumSubarray(arr, k){
     
-    let sum = 0
-    for (let i = 0; i <= arr.length - k; i++){
-        sum += arr[i]
-    }
-    return sum
-}
-
-
-//console.log(maxSumSubarray([2, 1, 5, 4, 3, 2], 2));  // Output: 9
-
-
-function minSubarrayWithSum(arr, s){
 
 }
 
 
+//console.log(maxSumSubarray([2, 1, 5, 4, 3, 2], 2)); // 4.5
 
 
-//console.log(minSubarrayWithSum([2, 1, 5, 2, 8], 7));  // Output: 1
+
+
+
+
